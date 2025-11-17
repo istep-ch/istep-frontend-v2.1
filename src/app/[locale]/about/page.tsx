ce7c4fbca3a1ad792d7ff8dd6c927185e983fa99
Yannic Reust <62382435+Yannic-reust@users.noreply.github.com>
2025-11-17 09:00:47 +0100

    feat: wip migration

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,34 +5,105 @@ import TeamsOverview from "@/components/about/teamsOverview/teamsOverview";
 import OurVision from "@/components/general/ourVision/ourVision";
 import OurValues from "@/components/general/ourValues/ourValues";
 
+import { gql, request } from "graphql-request";
+import { getLocale } from "next-intl/server";
+
 import ScrollDown from "@/components/general/scrollDown/scrollDown";
 
-import { useTranslations } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
+const endpoint =
+  "https://tqdbr6o1.api.sanity.io/v2023-08-01/graphql/develop/default";
 
-interface AboutUsProps {
-  params: {
-    locale: string;
-  };
+const fetchAboutPageQuery = gql`
+  query FetchAbout($language: String!) {
+    allAboutUsPage(where: { language: { eq: $language } }) {
+      _id
+      titlePreview {
+        title
+        subtitle
+        image {
+          asset {
+            url
+          }
+        }
+      }
+      whoAreWe {
+        title
+        subtitle
+        text
+        image {
+          asset {
+            url
+          }
+        }
+      }
+      teamOverview {
+        teamTitle
+        teamMembers {
+          title
+          text
+          image {
+            asset {
+              url
+            }
+          }
+        }
+      }
+      ourVision {
+        title
+        visionValuesItems {
+          title
+          text
+          image {
+            asset {
+              url
+            }
+          }
+        }
+      }
+      ourValues {
+        _id
+        title
+        visionValuesItems {
+          title
+          text
+          image {
+            asset {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+async function fetchAbout(language: string) {
+  try {
+    const data: any = await request(endpoint, fetchAboutPageQuery, {
+      language,
+    });
+    return data.allAboutUsPage ?? [];
+  } catch (error) {
+    console.error("GraphQL fetch error:", error);
+    return [];
+  }
 }
 
-export default function AboutUs({ params: { locale } }: AboutUsProps) {
-  setRequestLocale(locale);
-
-  const t = useTranslations("About");
-
+export default async function AboutUs({}) {
+  const locale = await getLocale();
+  const aboutData = await fetchAbout(locale);
   return (
     <main className="">
       <div className="min-h-[calc(100vh-80px)] mt-[80px] md:mt-0 md:min-h-[calc(100vh-0px)] bg-darkblue w-full flex items-center pb-8 md:-pd-0">
         <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4 pt-32 ">
           <h1 className="lg:text-h-xl  text-h-l  text-yellow font-palanquin md:text-left text-center">
-            {t("mainTitle")}
+            {aboutData[0]?.titlePreview?.title}
           </h1>
 
           <div className="flex flex-col lg:flex-row">
             <div className="lg:w-2/5 w-full">
               <h4 className="text-white text-h-sm md:text-h-md mt-4 mb-4  md:text-left text-center font-palanquin !font-thin">
-                {t("subTitle")}
+                {aboutData[0]?.titlePreview?.subtitle}
               </h4>
             </div>
             <div className="lg:w-3/5 w-full flex justify-center">
@@ -50,17 +121,17 @@ export default function AboutUs({ params: { locale } }: AboutUsProps) {
       </div>
 
       <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4">
-        <HowIsIstep lng={locale} />
+        <HowIsIstep whoAreWe={aboutData[0]?.whoAreWe} />
       </div>
-      <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4">
+      {/* <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4">
         <TeamsOverview lng={locale} />
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <OurVision lng={locale} />
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <OurValues lng={locale} />
-      </div>
+      </div> */}
     </main>
   );
 }
--- src/components/about/howIsIstep/howIsIstep.tsx
+++ src/components/about/howIsIstep/howIsIstep.tsx
@@ -1,38 +1,28 @@
 import Image from "next/image";
-import howIs from "@/assets/aboutUs/howIs.png";
 
-import { useTranslations } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
-
-interface whoIsProps {
-  lng: string;
-}
-
-export default async function howIsIstep({ lng }: whoIsProps) {
-  setRequestLocale(lng);
-  const t = useTranslations("About.whoIs");
-
+export default async function howIsIstep({ whoAreWe }: any) {
   return (
     <>
       <div className={`flex flex-col md:flex-row `}>
         <div className="font-palanquin w-full md:w-1/2">
           <h3 className={`text-h-md md:text-h-lg text-orange mb-2 md:mb-4`}>
-            {t("title")}
+            {whoAreWe.title}
           </h3>
           <p
             className={`text-p-lg md:text-h-md text-orange w-full md:w-4/5 mb-2 md:mb-4`}
           >
-            {t("subTitle")}
+            {whoAreWe.subTitle}
           </p>
           <p className="w-full mb-2 text-darkblue text-p-sm md:text-p-lg">
-            {t("text")}
+            {whoAreWe.text}
           </p>
         </div>
         <div className="w-full md:w-1/2 flex justify-center">
           <Image
-            src={howIs}
+            src={whoAreWe.image.asset.url}
             alt="Picture of the author"
             width={400}
+            height={400}
             className="p-4 object-contain"
           />
         </div>
